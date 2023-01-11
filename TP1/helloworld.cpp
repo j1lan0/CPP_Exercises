a1b17aa393888b5673b3a57e6624e3e5045eb4b7
Nico <harrylamagie@icloud.com>
2023-01-11 23:20:28 +0100

    chaine de caractères

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    int var;
+    std::string var;
+    std::cout << "What's your name?" << std::endl;
     std::cin >> var;
-    std::cout << "Hello world!" << std::endl;
+    std::cout << "Hello " << var << "!" << std::endl;
     return 0;
 }
