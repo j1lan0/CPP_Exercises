d3e8db1285ad5640d4a265e9566087727d48da08
BARDIN NICOLAS <nicolas.bardin@heckel02.u-pem.fr>
2023-01-20 10:29:23 +0100

    tp2 fini

--- src/translator.cpp
+++ src/translator.cpp
@@ -5,13 +5,13 @@
 #include <sstream>
 #include <string>
 #include <vector>
-using namespace std;
-map<string, string> trans;
-vector<string> history;
-set<std::string>
+
+std::map<std::string, std::string> trans;
+std::vector<std::string> history;
+std::set<std::string>
 make_exit_commands()
 {
-    set<std::string> quit;
+    std::set<std::string> quit;
     quit.emplace("e");
     quit.emplace("quit");
     quit.emplace("q");
@@ -19,122 +19,106 @@ make_exit_commands()
     return quit;
 }
 
-void add(string w1, string w2)
+
+void add(std::string w1, std::string w2)
 {
     trans.emplace(w1, w2);
     history.push_back("add " + w1 + " " + w2);
-    cout << w1 << "=>" << w2 << endl;
+    std::cout << w1 << "=>" << w2 << std::endl;
+}
+bool run_command(std::istream& input){
+    std::set<std::string> quit = make_exit_commands();
+    auto command = std::string{};
+    input >> command;
+    if (command == "add")
+    {
+        auto w1 = std::string{};
+        auto w2 = std::string{};
+        input >> w1;
+        input >> w2;
+        add(w1, w2);
+    }
+    else if (command == "translate")
+    {
+        auto line = std::string{};
+        getline(input, line);
+        auto words = std::stringstream{};
+        words << line;
+        auto key = std::string{};
+        while (words >> key)
+        {
+            const auto it = trans.find(key);
+            std::cout << (it == trans.end() ? "???" : it->second) << std::endl;
+        }
+        std::cout << std::endl;
+    }
+    else if (command == "save")
+    {
+        auto name = std::string{};
+        input >> name;
+        auto file = std::ofstream{name};
+        for (const auto &add : history)
+        {
+            file << add << "\n";
+        }
+    }
+    else if (command == "print")
+    {
+        for (const auto &[key, value] : trans)
+        {
+            std::cout << key << " => " << value << std::endl;
+        }
+    }
+    else if (command == "load")
+    {
+        auto name = std::string{};
+        input >> name;
+        auto file = std::ifstream{name};
+        std::vector<std::string> tmp;
+        while (!file.eof())
+        {
+            run_command(file);
+        }
+
+    }
+    else if (command == "clear")
+    {
+        trans.clear();
+        history.clear();
+
+    }
+    else if (command == "remove")
+    {
+        auto key = std::string{};
+        input >> key;
+        for (auto it = trans.begin(); it != trans.end();)
+        {
+            if (it->first == key || it->second == key)
+            {
+                std::cout << it->first << " " << it->second << std::endl;
+                it = trans.erase(it);
+            }
+            else
+            {
+                ++it;
+            }
+        }
+        history.push_back("remove " + key);
+    }
+    else if (quit.count(command) == 1)
+    {
+        return false;
+    }
+    return true;
 }
 int main()
 {
-    set<string> quit = make_exit_commands();
+    
     while (true)
     {
-        cout << "This is a translator, but it does nothing for now..." << endl;
-        string tmp;
-        auto command = string{};
-        cin >> command;
-        if (command == "add")
-        {
-            auto w1 = string{};
-            auto w2 = string{};
-            cin >> w1;
-            cin >> w2;
-            add(w1, w2);
-        }
-        else if (command == "translate")
-        {
-            auto line = std::string{};
-            getline(cin, line);
-            auto words = std::stringstream{};
-            words << line;
-            while (!words.eof())
-            {
-                auto key = string{};
-                words >> key;
-                const auto it = trans.find(key);
-                std::cout << (it == trans.end() ? "???" : it->second) << endl;
-            }
-            std::cout << std::endl;
-        }
-        else if (command == "save")
-        {
-            auto name = string{};
-            cin >> name;
-            auto file = ofstream{name};
-            for (const auto &add : history)
-            {
-                file << add << "\n";
-            }
-        }
-        else if (command == "print")
-        {
-            for (const auto &[key, value] : trans)
-            {
-                std::cout << key << " => " << value << std::endl;
-            }
-        }
-        else if (command == "load")
-        {
-            auto name = string{};
-            cin >> name;
-            auto file = ifstream{name};
-            vector<string> tmp;
-            while (!file.eof())
-            {
-                auto key = string{};
-                file >> key;
-                if (key == "add")
-                {
-                    if (tmp.size() == 2)
-                    {
-                        add(tmp[0], tmp[1]);
-                        while (!tmp.empty())
-                        {
-                            tmp.pop_back();
-                        }
-                    }
-                    continue;
-                }
-                else if (tmp.size() < 2)
-                {
-                    tmp.emplace_back(key);
-                }
-            }
-            if (tmp.size() == 2)
-            {
-                add(tmp[0], tmp[1]);
-                while (!tmp.empty())
-                {
-                    tmp.pop_back();
-                }
-            }
-        }
-        else if (command == "clear")
-        {
-            trans.clear();
-        }
-        else if (command == "remove")
-        {
-            auto key = string{};
-            cin >> key;
-            for (auto it = trans.begin(); it != trans.end();)
-            {
-                if (it->first == key || it->second == key)
-                {
-                    cout << it->first << " " << it->second << endl;
-                    it = trans.erase(it);
-                }
-                else
-                {
-                    ++it;
-                }
-            }
-        }
-        else if (quit.count(tmp) == 1)
-        {
+        if(!run_command(std::cin)){
             return 0;
-        }
+        } 
     }
 
     return 0;
