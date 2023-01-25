351f61e72c9e41ade9e3e78d4a7d058d64f0838c
Nico <harrylamagie@icloud.com>
2023-01-26 00:00:54 +0100

    tp3 question 1 a 10

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -1,1 +1,23 @@
 #pragma once
+
+class Plush
+{
+public:
+    Plush(int cost) : _cost{cost}
+    {
+    }
+    Plush()
+    {
+    }
+    int get_cost() const
+    {
+        return _cost;
+    }
+    void set_cost(int cost)
+    {
+        _cost = cost;
+    }
+
+private:
+    int _cost = 10;
+};
--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,1 +1,63 @@
 #pragma once
+#include <string>
+
+class PlushStore
+{
+public:
+    PlushStore(const std::string &name) : _name{name}
+    {
+    }
+    const std::string &get_name() const
+    {
+        return _name;
+    }
+    int get_wealth_amount() const
+    {
+        return _money;
+    }
+    int get_stock_size() const
+    {
+        return _stock;
+    }
+    void loan(int cost)
+    {
+        _money += cost;
+        _debt += cost * 1.1;
+    }
+    int get_debt_amount() const
+    {
+        return _debt;
+    }
+    int make_plush(int cost)
+    {
+        int invest = 0;
+        if (_money < cost)
+        {
+            invest = _money;
+        }
+        invest = cost;
+        if (_money == 0)
+        {
+            return 0;
+        }
+        _money -= cost;
+        if (_money <= 0)
+        {
+            _money = 0;
+        }
+        _stock += 1;
+        _experience += 1;
+        return invest + _experience;
+    }
+    int get_experience() const
+    {
+        return _experience;
+    }
+
+private:
+    std::string _name{};
+    int _money = 0;
+    int _stock = 0;
+    int _debt = 0;
+    int _experience = 0;
+};
